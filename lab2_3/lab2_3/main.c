d8eca00aeb0d3afce36e8ba7ca29cc33edfa3fbf
Oskar Norin <oskar.norin93@gmail.com>
2026-02-09 21:01:59 +0100

    checkpoint

---
+++ lab2_3/lab2_3/main.c
@@ -0,0 +1,18 @@
+/*
+ * lab2_3.c
+ *
+ * Created: 2026-02-09 18:23:58
+ * Author : Oskar
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

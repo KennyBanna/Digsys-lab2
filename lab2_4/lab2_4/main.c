869f9ae26df0e8a13dafc50e9ad2851f30f4fb3f
Oskar Norin <oskar.norin93@gmail.com>
2026-02-12 15:05:26 +0100

    lab2.4 now properly reads analog to digital

--- lab2_4/lab2_4/main.c
+++ lab2_4/lab2_4/main.c
@@ -30,6 +30,8 @@ void set_adlar_to_right(){
 	ADMUX &= ~(1 << 5);
 }
 
+
+// TODO: Avcc is tp supply voltage from a seperate pin
 void set_ADC_to_use_AVcc(){
 	ADMUX &= ~(1 << 7);
 	ADMUX |= (1 << 6);
@@ -44,16 +46,35 @@ void adc_init(){
 }
 
 uint16_t read_adc(){
+	ADCSRA |= (1 << ADSC);	// set start conversion bit to 1
+	while ((ADCSRA & (1 << ADSC)) != 0) {} // wait until conversation is done
 	
+	uint16_t data = ADCH;
+	data = data << 8;
+	data |= ADCL;
+	
+	uint8_t low = ADCL; // DEBUG purposes only
+	uint8_t high = ADCH; // Debug purposes only
+	
+	return data;
 }
 
 int main(void)
 {
-	void adc_init();
+	DDRB = 0b11111111;
+	PORTB |= (1 << 0);
+	adc_init();
+	
 	
     /* Replace with your application code */
-    while (1) 
+    while (1)
     {
-    }
+		
+ 		if(read_adc() > 500){
+ 			PORTB = 0x00;
+ 		} else {
+ 			PORTB = 0xff;
+ 		}
+	}
 }
 
